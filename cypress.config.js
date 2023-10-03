8fdcb74a6864b3aa2ab36f1a4fb057a3a952268f
Sureshc-AdapptLimited <suresh.c@adappt.ai>
2023-10-03 17:38:58 +0530

    Working site launch issue#1

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://gifna-stage.who.int",
+    //baseUrl: "https://gifna-stage.who.int",
   },
 });
